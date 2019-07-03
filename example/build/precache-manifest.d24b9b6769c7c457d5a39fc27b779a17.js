3cca85a5df906e825839cd362174fe0052366f37
Ansuman Ghosh <ansumanghosh@infrrd.ai>
2019-07-04 01:58:56 +0530

    Updated examples

---
+++ example/build/precache-manifest.d24b9b6769c7c457d5a39fc27b779a17.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8c08dd61bf11868361e5df2b0bb899b",
+    "url": "/pdf-viewer-reactjs/index.html"
+  },
+  {
+    "revision": "af1bb0e96d8d4e0c66c1",
+    "url": "/pdf-viewer-reactjs/static/css/2.770b7292.chunk.css"
+  },
+  {
+    "revision": "492b0377e5d1405116a3",
+    "url": "/pdf-viewer-reactjs/static/css/main.3eba250e.chunk.css"
+  },
+  {
+    "revision": "af1bb0e96d8d4e0c66c1",
+    "url": "/pdf-viewer-reactjs/static/js/2.0eaaa74b.chunk.js"
+  },
+  {
+    "revision": "492b0377e5d1405116a3",
+    "url": "/pdf-viewer-reactjs/static/js/main.22acc4b3.chunk.js"
+  },
+  {
+    "revision": "a3d0202a3f7213b4a8da",
+    "url": "/pdf-viewer-reactjs/static/js/runtime~main.b5664e04.js"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "/pdf-viewer-reactjs/static/media/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "/pdf-viewer-reactjs/static/media/MaterialIcons-Regular.570eb838.woff2"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/pdf-viewer-reactjs/static/media/MaterialIcons-Regular.a37b0c01.ttf"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "/pdf-viewer-reactjs/static/media/MaterialIcons-Regular.e79bfd88.eot"
+  }
+]);
