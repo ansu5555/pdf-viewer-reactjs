16a3fe7cdd753ec6f7459c325bc5c2e755181c7d
ansuman5555 <ansumanghosh@infrrd.ai>
2020-09-05 17:41:26 +0530

    upgrade babel and rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,5 @@
-import babel from 'rollup-plugin-babel'
-import { uglify } from 'rollup-plugin-uglify'
+import babel from '@rollup/plugin-babel'
+import { terser } from 'rollup-plugin-terser'
 
 export default {
     input: 'src/index.js',
@@ -19,9 +19,9 @@ export default {
     ],
     plugins: [
         babel({
-            exclude: 'node_modules/**',
-            runtimeHelpers: true,
+            babelHelpers: 'runtime',
+            exclude: '/node_modules/',
         }),
-        uglify(),
+        terser(),
     ],
 }
