197eca7a9d527853e96179934937ce2acaf1d7ac
ansuman5555 <ansumanghosh@infrrd.ai>
2021-01-27 11:27:21 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pdf-viewer-reactjs/precache-manifest.8aca9e36d581c833d0b1e2a335cf3614.js"
+  "/pdf-viewer-reactjs/precache-manifest.2891eae50222f1b877781ca81f235393.js"
 );
 
 self.addEventListener('message', (event) => {
