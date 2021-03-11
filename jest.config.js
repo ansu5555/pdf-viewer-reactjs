468cb38f3df203ea2791d6958d841e7563dc22ca
ansu5555 <ansuman5555@gmail.com>
2021-03-11 10:50:47 +0530

    Added test steps for CI

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   coverageDirectory: 'coverage',
+  collectCoverage: true,
   setupFilesAfterEnv: ['./setupTests.js'],
   testMatch: ['**/__tests__/**/*spec.js?(x)'],
   testPathIgnorePatterns: ['/node_modules/', '/example/'],
